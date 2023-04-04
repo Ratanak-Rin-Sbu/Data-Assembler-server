000d4fac85597b0a24f016fabd9a7cdfc2fb0a3b
Ratanak-Rin-Sbu <100118148+Ratanak-Rin-Sbu@users.noreply.github.com>
2023-04-05 00:00:43 +0700

    MongoDB database setup

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import motor.motor_asyncio
+import asyncio
+import motor.core
 
 app = FastAPI(
     title="DataAssembler",
 )
+
+# DATABASE SETUP
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://jassonrin:stfuimissHER0730@cluster0.z587qgx.mongodb.net/?retryWrites=true&w=majority')
+client.get_io_loop = asyncio.get_running_loop
+# User DB
+userDB = client.UserList
+userCollection = userDB.user
 
 # CORS SETUP
 origins = [
