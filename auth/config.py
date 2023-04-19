31e123593975b0bdc9096e932e5b62071b5fc6bb
Ratanak-Rin-Sbu <100118148+Ratanak-Rin-Sbu@users.noreply.github.com>
2023-04-19 22:18:09 +0700

    Added hash pw alg, verify pw, jwt, bcrypt, tokens configuration

---
+++ auth/config.py
@@ -0,0 +1,21 @@
+from typing import List
+from decouple import config
+from pydantic import AnyHttpUrl, BaseSettings
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api"
+    DB_URL: str = config("DB_URL", cast=str)
+    JWT_SECRET_KEY: str = config("JWT_SECRET_KEY", cast=str)
+    JWT_REFRESH_SECRET_KEY: str = config("JWT_REFRESH_SECRET_KEY", cast=str)
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7   # 7 days
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
+        "http://localhost:3000"
+    ]
+    PROJECT_NAME: str = "DataAssembler"
+    
+    class Config:
+        case_sensitive = True
+        
+settings = Settings()
